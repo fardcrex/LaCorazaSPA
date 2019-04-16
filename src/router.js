7ab28d8428d54015c6e67c598bc39504e0faf837
Jair Conislla <1632874@utp.edu.pe>
2019-04-16 11:52:35 -0500

    feat Login
    Se agregan el registro

--- src/router.js
+++ src/router.js
@@ -1,25 +1,145 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+  import Login from './components/Home/Login.vue'
+  import Registro from './components/Home/Registro.vue'
+  import Welcome from './components/Home/Welcome.vue'
+import Black from './views/Black.vue'
+import Premium from './views/Premium.vue'
+import Verano from './views/Verano.vue'
+import muggle from './views/muggle.vue'
+import otonio from './views/Otonio.vue'
+import Perfil from './views/Perfil.vue'
+import About from './views/About.vue'
+import store from './store';
+import axios from 'axios'
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect:'/Home'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+      path: '/Home',
+      name: 'Home',
+      component: Home,
+      redirect:'/Home',
+      children:[
+        
+        {
+          path:'Login', component:Login,alias: '/Ingreso',
+        },
+        {
+          path:'Registro', component:Registro
+        },
+        {
+          path:'', component:Welcome,
+        }
+      ]
+    },
+    {
+      path: '/About',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/Black',
+      name: 'Black',      
+      component: Black,
+      meta:{
+        autenticado: true
+      }
+    },
+    {
+      path: '/Premium',
+      name: 'Premium',
+      component: Premium,
+      meta:{
+        autenticado: true
+      }
+    },
+    {
+      path: '/Verano',
+      name: 'Verano',
+      component: Verano,
+      meta:{
+        autenticado: true
+      }
+    },
+    {
+      path: '/Muggle',
+      name: 'Muggle',      
+      component: muggle,
+      meta:{
+        autenticado: true
+      }
+    },
+    {
+      path: '/Otoño',
+      name: 'Otoño',      
+      component: otonio,
+      meta:{
+        autenticado: true
+      }
+    },    
+    {
+      path: '/Perfil',
+      name: 'Perfil',      
+      component: Perfil,
+      meta:{
+        autenticado: true
+      }
+    }]
 })
+router.beforeEach(async (to,from,next)=>{  
+  if(to.matched.some( (record) => record.meta.autenticado)){
+    if(!localStorage.token){
+      console.log(`no token`); 
+      next({name:'Home'})
+    }else if(store.state.usuario){
+      console.log(`si usuario`);
+      next()
+    }else{      
+      console.log(`no usuario`);
+      let token = localStorage.token      
+      axios.defaults.headers.common['Authorization'] =`Bearer ${token}`
+      axios.get(`http://www.lacoraza.com:8080/TokenServer/v1/opciones`).
+      then( response=>{        
+      //  console.log(to);
+        if(response.status==202){
+          if(to.name==="Perfil"){
+            console.log(`no ////////`);
+            next()
+          }else{
+          console.log(`no /////zzzzzz///`);
+          let dSuscripciones=response.data.reduce(function (obj, item) { 
+            obj[item.name] = item; 
+            return obj; }, {});
+          if(dSuscripciones[to.name]){
+            next()
+          }else{
+            next({name:'Home'})
+          }}                 
+        }else{
+          localStorage.clear()
+          next({name:'Home'})
+        }})
+      .catch(error=> {
+        localStorage.clear()
+        next({name:'Home'})
+      })
+    }  
+    console.log(`router`);    
+    
+  }else{
+    next()
+  }
+})
+
+
+
+export default router
--- src/store.js
+++ src/store.js
@@ -1,16 +1,60 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
+import router from './router'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    usuario:null,
+    menu:[{id:0,name:"Home"},{id:-1,name:"About"}],
+    fondo:"normal"
   },
   mutations: {
-
+    actualizarUsuario(state,usuario){
+      state.usuario= usuario
+    },
+    actualizarMenu(state,menu){
+      state.menu = [...[{id:0,name:"Perfil"},{id:-1,name:"About"}],...menu]
+    },
+    regresarMenu(state){
+      state.menu = [{id:0,name:"Home"},{id:-1,name:"About"}]
+    },
+    actualizarFondo(state,fondo){
+      state.fondo= fondo
+    }
+    
   },
   actions: {
+    async getDataUsuarios({commit,state}){
 
+      console.log(`store`);
+        let token = localStorage.token,
+            usuario = JSON.parse(localStorage.usuario)
+            
+      
+        axios.defaults.headers.common['Authorization'] =`Bearer ${token}`  
+        axios.all([
+          axios.get(`http://www.lacoraza.com:8080/TokenServer/v1/opciones`),
+          axios.get(`http://www.lacoraza.com:8080/TokenServer/v1/usuarios/${usuario.id}`)
+        ])
+        .then(axios.spread((response1, response2) => {
+          
+          
+          if(response2.data.nombre){
+            
+            commit('actualizarMenu',response1.data)
+            commit('actualizarUsuario',response2.data)
+          }
+          if(!localStorage.Menu){
+            localStorage.Menu = JSON.stringify(response1.data)
+            router.push({name:"Perfil"})   
+          }  
+          localStorage.Menu = JSON.stringify(response1.data)
+        }))
+        .catch(error => {
+            console.log(error);
+        }) 
+    }
   }
 })
